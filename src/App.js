aa476e7d37f08cc1825e53d028a7c727467b5779
ntprdk <nuttaphonrod@gmail.com>
2021-04-21 14:43:04 +0700

    add clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './components/User'
+import Clock from './components/Clock'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello React
-      </header>
+      <ul className="App-header">
+        <User info="01" age="55"></User>
+        <User info="02"></User>
+        <User info="03"></User>
+        <Clock />
+      </ul>
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,8 @@
+function User(props) {
+    return (
+        <li>User {props.info} - {props.age}</li>
+    )
+}
+
+
+export default User
